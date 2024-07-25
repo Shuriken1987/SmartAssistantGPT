e144a8bdb7093516966cacf91c9655e13871390d
shimashimau <shuriken233@gmail.com>
2024-07-25 20:20:19 +0200

    feat: Encapsulate mobile sidebar

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
-import { setMessage, setTempValue, setValue, addChat, setCurrentTitle, } from "../../redux/slices/chatSlice";
+import {
+  setMessage,
+  setTempValue,
+  setValue,
+  addChat,
+  setCurrentTitle,
+} from "../../redux/slices/chatSlice";
 import ToolbarButtons from "./ToolbarButtons";
 import TextArea from "./TextArea";
+import { SidebarMobileButton } from "../Sidebar/SidebarMobileButton";
 
 export const Input: React.FC = () => {
   const dispatch = useDispatch();
-  const { value, currentTitle } = useSelector((state: RootState) => state.chat);  // Importing value from Redux store
+  const { value, currentTitle } = useSelector((state: RootState) => state.chat); // Importing value from Redux store
   const [isRecording, setIsRecording] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>( null );
 
   const startRecording = async () => {
     setIsRecording(true);
@@ -64,17 +71,29 @@ export const Input: React.FC = () => {
         dispatch(setCurrentTitle(data?.transcribedText.content));
       }
       const newTitle = currentTitle || data?.transcribedText.content;
-      dispatch(addChat({ title: newTitle, role: "user", content: data?.transcribedText.content })); // User transcribed text response
+      dispatch(
+        addChat({
+          title: newTitle,
+          role: "user",
+          content: data?.transcribedText.content,
+        })
+      ); // User transcribed text response
       dispatch(setMessage(data?.chatResponseText));
-      dispatch(addChat({ title: newTitle, role: "assistant", content: data?.chatResponseText.content })); // Chatbot response
+      dispatch(
+        addChat({
+          title: newTitle,
+          role: "assistant",
+          content: data?.chatResponseText.content,
+        })
+      ); // Chatbot response
     } catch (error) {
       console.error("Error in handleAudioUpload:", error);
     }
   };
 
   const getMessages = async () => {
-     // Immediately update the chat with the user's input
-     if (!currentTitle) {
+    // Immediately update the chat with the user's input
+    if (!currentTitle) {
       dispatch(setCurrentTitle(value));
     }
     const newChatTitle = currentTitle || value;
@@ -88,9 +107,18 @@ export const Input: React.FC = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8000/completions", options);
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
       const data = await response.json();
-      dispatch(addChat({ title: newChatTitle, role: data.choices[0].message.role, content: data.choices[0].message.content })); // Check for undefined
+      dispatch(
+        addChat({
+          title: newChatTitle,
+          role: data.choices[0].message.role,
+          content: data.choices[0].message.content,
+        })
+      ); // Check for undefined
       dispatch(setMessage(data.choices ? data.choices[0].message : "")); // Check for undefined
       dispatch(setTempValue(value));
     } catch (error) {
@@ -101,36 +129,7 @@ export const Input: React.FC = () => {
   return (
     <>
       <footer className="max-w-4xl mx-auto sticky bottom-0 z-10 p-3 sm:py-6">
-        {/* Sidebar Button Mobile */}
-        <div className="lg:hidden flex justify-end mb-2 sm:mb-3">
-          <button
-            type="button"
-            className="p-2 inline-flex items-center gap-x-2 text-xs font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-            data-hs-overlay="#application-sidebar"
-            aria-controls="application-sidebar"
-            aria-label="Toggle navigation"
-          >
-            <svg
-              className="flex-shrink-0 size-3.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <line x1="3" x2="21" y1="6" y2="6" />
-              <line x1="3" x2="21" y1="12" y2="12" />
-              <line x1="3" x2="21" y1="18" y2="18" />
-            </svg>
-            <span>Sidebar</span>
-          </button>
-        </div>
-        {/* End Sidebar Button Mobile */}
-
+        <SidebarMobileButton />
         <div className="relative">
           <TextArea />
           <ToolbarButtons
---
+++ client/src/components/Sidebar/SidebarMobileButton.tsx
@@ -0,0 +1,31 @@
+export const SidebarMobileButton = () => {
+  return (
+    <div className="lg:hidden flex justify-end mb-2 sm:mb-3">
+      <button
+        type="button"
+        className="p-2 inline-flex items-center gap-x-2 text-xs font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+        data-hs-overlay="#application-sidebar"
+        aria-controls="application-sidebar"
+        aria-label="Toggle navigation"
+      >
+        <svg
+          className="flex-shrink-0 size-3.5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="3" x2="21" y1="6" y2="6" />
+          <line x1="3" x2="21" y1="12" y2="12" />
+          <line x1="3" x2="21" y1="18" y2="18" />
+        </svg>
+        <span>Sidebar</span>
+      </button>
+    </div>
+  );
+};
